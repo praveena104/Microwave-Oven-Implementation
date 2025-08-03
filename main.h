c9d2377c06479787775b9b97daeab406a3a3feff
Praveena <mpraveena104@gmail.com>
2025-08-03 14:27:53 +0530

    main.h

--- main.h
+++ main.h
@@ -8,34 +8,41 @@
 #ifndef MAIN_H
 #define	MAIN_H
 
+
+#include <xc.h>
 #include"clcd.h"
 #include "matrix_keypad.h"
 #include "microwave_def.h"
 #include "timers.h"
 
-#include <xc.h>
 
-#define POWER_ON_SCREEN  0x01
-#define MENU_SCREEN      0x02
-#define MICRO_MODE       0x11
-#define GRILL_MODE       0x12
-#define CONVECTION_MODE  0x13
-#define START_MODE       0x14
-#define RESET_NOTHING    0x00
-#define RESET_MODE       0x04
-#define DISPLAY_TIME     0xDD
-#define TIMER_SCREEN     0x03
 
-#define START           0xAA
-#define STOP            0xBB
-#define PAUSE           0xCC
-#define PRE_HEAT        0xEE
-#define ENTER           0xFF
+#define SUCCESS             0x00
+#define FAILURE             0xFF
 
-#define FAN_DDR         TRISC2
-#define FAN             RC2
+#define POWER_ON_SCREEN     0x01
+#define MENU_SCREEN         0x02
 
-#define BUZZER_DDR      TRISC1
-#define BUZZER          RC1
+#define MICRO_MODE          0x11
+#define GRILL_MODE          0x12
+#define CONVECTION_MODE     0x13
+#define START_MODE          0x14
+
+#define RESET_NOTHING       0x22
+#define RESET_MODE          0x33
+#define DISPLAY_TIME        0x44
+#define OPERATION_MODE      0x55
+
+#define START               0xAA
+#define STOP                0xBB
+#define PAUSE               0xCC
+#define PRE_HEAT            0xDD
+
+#define BUZZER_DDR          TRISC1
+#define BUZZER              RC1
+
+#define FAN_DDR             TRISC2
+#define FAN                 RC2
+ 
 #endif	
 
