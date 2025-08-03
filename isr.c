715875c840b4132f440855737b9ed913230fa671
Praveena <mpraveena104@gmail.com>
2025-08-03 14:27:00 +0530

    isr.c

--- isr.c
+++ isr.c
@@ -1,23 +1,26 @@
 #include <xc.h>
 #include "main.h"
-extern volatile unsigned int remaining_time;
+
+extern unsigned char sec,min,pre_time;
 
 void __interrupt() isr(void)
 {
     static unsigned int count = 0;
     
     if (TMR2IF == 1)
-    { 
-        
-        //1;16 prescale 
+    {
+        // 1:16 prescale
         if (++count == 1250)
         {
-            if (remaining_time > 0)
-            {
-                remaining_time--; // Decrement remaining time
-            }
-          
             count = 0;
+            if(sec-- == 0 && min >= 0)
+            {
+                sec = 59;
+                min--;
+            }
+            
+            if(pre_time > 0)
+                pre_time--;
         }
         
         TMR2IF = 0;
